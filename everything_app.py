d7528389799e76b7fa8ec878dda02746b1fd045b
gdl <chinagaodalii@gmail.com>
2024-02-27 16:36:22 +0800

    backup

--- everything_app.py
+++ everything_app.py
@@ -11,18 +11,14 @@ def test(input_image):
 with gr.Blocks(theme = gr.themes.Soft()) as demo:      
     with gr.Column():    
         image_component = gr.Image(type="numpy")               
-        with gr.Tab(label="Everything") as seg_everything_tab:  
-            seg_everything_btn = gr.Button()                                             
-        with gr.Tab(label="Box") as box_tab:  
-            pass            
-                                             
+        seg_everything_btn = gr.Button()                                                      
+                                                     
     seg_everything_btn.click(
         fn=seg_everything,
         inputs=[image_component],
         outputs=[image_component],
     )                                                                        
-
-                        
+                     
 demo.queue(max_size=5)  
 
 if __name__ == "__main__":     
